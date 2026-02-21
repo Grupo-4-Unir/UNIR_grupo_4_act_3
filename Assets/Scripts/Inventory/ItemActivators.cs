2c67174d0f736143520dbaf998fe561da4208dbe
Diego Fabian Salamanca Garcia <diegocolmayor@gmail.com>
2026-02-20 22:11:33 -0500

    Set door logic

--- Assets/Scripts/Inventory/ItemActivators.cs
+++ Assets/Scripts/Inventory/ItemActivators.cs
@@ -4,27 +4,67 @@ using UnityEngine.Events;
 public class ItemActivators : MonoBehaviour
 {
 
-    public string itemName;
+    public string itemName, goodMessage, badmMssage;
+
+    public bool requireItem;
+
+    [SerializeField]
+    TMPro.TextMeshProUGUI messageText;
 
     [SerializeField]
     InventoryScriptable inventoryScriptable;
+
+    [SerializeField]
+    CanvasGroup canvasGroup;
+
+    [SerializeField]
+    float fadeTime;
 
     public UnityEvent onItemActivated;
 
     public void CheckItem()
     {
-        var containsItem = false;
-
-        foreach (var item in inventoryScriptable.items)
+        if (!requireItem)
         {
-            if (itemName == item.itemName)
-                containsItem = true;
+            onItemActivated.Invoke();
+            return;
         }
 
-        if (containsItem)
-            onItemActivated.Invoke();
+        if (!inventoryScriptable.itemActive)
+        {
+            SetMessage(badmMssage);
+            return;
+        }
+
+        if (inventoryScriptable.itemActive.itemName != itemName)
+        {
+            SetMessage(badmMssage);
+            return;
+        }
+
+
+        SetMessage(goodMessage);
+        onItemActivated.Invoke();
+    }
+
+    private void Update()
+    {
+        if (!canvasGroup)
+            return;
+
+        if(canvasGroup.alpha>0)
+        canvasGroup.alpha -= fadeTime * Time.deltaTime;
+    }
+
+    public void SetMessage(string message)
+    {
+        if (message.Length == 0 || !canvasGroup)
+            return;
+        
+        messageText.text = message;
+        canvasGroup.alpha = 1;
     }
 
 
-    
+
 }
